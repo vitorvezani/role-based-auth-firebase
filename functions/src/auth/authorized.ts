9969756260940df2c029e11512058e1680e41533
Vitor Rodrigo Vezani <vitor.vezani@freshcells.de>
2019-08-29 13:17:02 +0200

    Add missing endpoints

--- functions/src/auth/authorized.ts
+++ functions/src/auth/authorized.ts
@@ -2,17 +2,20 @@ import { Request, Response } from "express";
 
 export function isAuthorized(opts: { hasRole: Array<'admin' | 'manager' | 'user'>, allowSameUser?: boolean }) {
    return (req: Request, res: Response, next: Function) => {
-       const { role, uid } = res.locals
+       const { role, email, uid } = res.locals
        const { id } = req.params
 
+       if (email === 'vitor.vezani@gmail.com')
+            return next();
+
        if (opts.allowSameUser && id && uid === id)
-           return next();
+            return next();
 
        if (!role)
-           return res.status(403).send();
+            return res.status(403).send();
 
-       if (opts.hasRole.includes(role))
-           return next();
+        if (opts.hasRole.includes(role))
+            return next();
 
        return res.status(403).send();
    }
