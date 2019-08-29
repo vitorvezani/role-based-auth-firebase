5fde07f22a759d90a2d08d9a5eeb84cf1a14fbaa
Vitor Rodrigo Vezani <vitor.vezani@freshcells.de>
2019-08-29 12:04:24 +0200

    Add authentication and authorization

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,16 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+import { routesConfig } from './users/routes-config';
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
+admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+const app = express();
+app.use(bodyParser.json());
+app.use(cors({ origin: true }));
+
+routesConfig(app)
+
+export const api = functions.https.onRequest(app);
---
+++ functions/src/users/routes-config.ts
@@ -0,0 +1,11 @@
+import { Application } from "express";
+import { create } from "./controller";
+import { isAuthenticated } from "../auth/authenticated";
+import { isAuthorized } from "../auth/authorized";
+
+export function routesConfig(app: Application) {
+   app.post('/users', 
+   isAuthenticated,
+   isAuthorized({ hasRole: ['admin', 'manager'] }),
+   create );
+}
